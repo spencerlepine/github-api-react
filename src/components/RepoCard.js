eb0d0737e5576dc0a8c180a8bc7fb34a76f183b7
Spencer Lepine <60903378+spencerlepine@users.noreply.github.com>
2021-02-15 11:32:10 -0500

    refactor to functional component

--- src/components/RepoCard.js
+++ src/components/RepoCard.js
@@ -1,39 +1,32 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 
-class RepoCard extends Component {
-    constructor() {
-        super();
-        this.state = {
-            repoInfo: {},
-            owner: null
-        }
-    }
+function RepoCard(props) {
+    const [repoObj] = useState(props.repoInfo)
+    
+    return (
+        <div className="repoCard">
+                <p>
+                    {repoObj.full_name} - 
+                    <a href={`https://github.com/${repoObj.owner.login}/${repoObj.name}`} target="_blank">
+                        Source
+                    </a>
+                </p>
 
-    componentDidMount() {
-        // After the component mounts, save the prop data
-        this.setState({ repoInfo: this.props.repoInfo, owner: this.props.repoInfo.owner.login});
-    }
-
-    render() {
-        let repoObj = this.state.repoInfo;
-        return (
-            // This component is never created until server data is found
-            <div className="repoCard">
-                <p>{repoObj.full_name} - <a href={`https://github.com/${this.state.owner}/${repoObj.name}`} target="_blank">Source</a></p>
                 <hr/>
                 <p><b>Description: </b> {repoObj.description}</p>
-                <p><b>Created: </b> {repoObj.created_at}</p>    
+               
+                <p><b>Created: </b> {repoObj.created_at}</p>   
+
                 <div className="stats">
                     <p>Forks: {repoObj.forks}</p>
                     <p>Updated at: {repoObj.updated_at}</p>
                     <p>Open issues count: {repoObj.open_issues_count}</p>
                     <p>Watchers count: {repoObj.watchers_count}</p>
-                    <p>Size: {repoObj.size}</p>{/* Expressed in kilobytes */}
+                    <p>Size: {repoObj.size}</p>
                     <p>Stargazers: {repoObj.stargazers_count}</p>
                 </div>
-            </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default RepoCard
